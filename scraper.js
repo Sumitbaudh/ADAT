a71677844ea37cb2cb5dfebc7be664d8ccc54a6a
sumbaudh <sumitbaudh2205@gmail.com>
2025-02-26 22:20:07 +0530

    fix: updated script

--- scraper.js
+++ scraper.js
@@ -37,12 +37,18 @@ async function scrapeTender() {
       document.querySelectorAll("#activeTenders tr").forEach(row => {
         const columns = row.querySelectorAll("td");
         if (columns.length === 4) {
+          let link = columns[0]?.querySelector("a")?.href ? new URL(columns[0]?.querySelector("a")?.href) : null;
+          if(link){
+            link.searchParams.delete("session")
+            link = link.toString();
+          }
+          
           jobs.push({
             title: columns[0]?.innerText.trim(),
             referenceNo: columns[1]?.innerText.trim(),
             closingDate: columns[2]?.innerText.trim(),
             bidOpeningDate: columns[3]?.innerText.trim(),
-            link: columns[0]?.querySelector("a")?.href || null
+            link
           });
         }
       });
